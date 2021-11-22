305d158366cbbc4b7ec5adc43dbe1aecc8bcf8e1
Manav Singh <79736172+Manav-Singh-h@users.noreply.github.com>
2021-11-22 08:58:12 +0530

    Create test.c

---
+++ program to accept a number from user and frint it's square and cube/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int square,cube,a;
+    printf("\n Enter a number :"); scanf("%d",&a);
+    square= a*a;
+    cube = a*a*a;
+    printf("The Square & Cybe of the given number is %d & %d",square, cube);
+
+    return 0;
+
+}
