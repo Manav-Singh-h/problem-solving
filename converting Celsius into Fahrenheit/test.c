39963e5ea615bd164bee46e605a32398a5300433
Manav Singh <79736172+Manav-Singh-h@users.noreply.github.com>
2021-11-22 09:11:33 +0530

    Create test.c

---
+++ converting Celsius into Fahrenheit/test.c
@@ -0,0 +1,9 @@
+# include <stdio.h>
+int main()
+{
+    float celcius = 37,far;
+     far = (celcius*9/5)+32;
+    
+    printf("\n The value of this celcius temperature in far is %f",far);
+      return 0;
+}
