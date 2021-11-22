9a2069372f2d8ee3bb71555bbe32953411cd7ef2
Manav Singh <79736172+Manav-Singh-h@users.noreply.github.com>
2021-11-22 08:55:16 +0530

    Create test.c

---
+++ program to print area of a circle, A(circle)= 3.142*R*R in C/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    float A,pi= 3.142;
+    int R;
+    printf("\n Radius of a circle is :"); scanf("%d",&R);
+    A= 3.142*R*R;
+    printf("\n Area of a circle is : %f",A);
+
+    return 0;
+
+}
